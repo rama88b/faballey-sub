8f63aaef8c95fc7e60d897d66d2c62d588d0f9fb
root <root@vm1.us-central1-c.c.ramakk-gcp.internal>
2025-06-27 17:47:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: 'Segoe UI', sans-serif;
+            background: #f0f2f5;
+            text-align: center;
+            padding-top: 100px;
+          }
+          h1 {
+            font-size: 2.5em;
+            color: #4b0082;
+            font-weight: bold;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome sub domain of demo-byteeit.online</h1>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
